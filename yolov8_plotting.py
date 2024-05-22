407d2b9bf9c3466836659dd893f0d4d3cc3b9ce3
Ryan <rzhou058@gmail.com>
2024-05-22 20:34:53 +0800

    I submitted yesterday, but now I adjust few things

---
+++ yolov8_plotting.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+# Open the video file
+video_path = "argoverse.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# save 
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+output_path = "output.mp4"
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for .mp4 file
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+
+# Store the track history
+track_history = defaultdict(lambda: [])
+
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Get the boxes and track IDs
+        boxes = results[0].boxes.xywh.cpu()
+        track_ids = results[0].boxes.id.int().cpu().tolist()
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Plot the tracks
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            track = track_history[track_id]
+            track.append((float(x), float(y)))  # x, y center point
+            if len(track) > 30:  # retain 90 tracks for 90 frames
+                track.pop(0)
+
+            # Draw the tracking lines
+            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+
+        out.write(annotated_frame)
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
